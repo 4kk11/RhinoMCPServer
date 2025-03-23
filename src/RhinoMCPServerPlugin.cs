4ea7a47f0a76327328e18a5a16a8fb7bc4141a5b
4kk11 <akiaki118row@yahoo.co.jp>
2025-03-23 14:00:24 +0900

    WIP

---
+++ src/RhinoMCPServerPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rhino;
+
+namespace RhinoMCPServer
+{
+    public class RhinoMCPServerPlugin : Rhino.PlugIns.PlugIn
+    {
+        public RhinoMCPServerPlugin()
+        {
+            Instance = this;
+        }
+        
+        public static RhinoMCPServerPlugin Instance { get; private set; }
+    }
+}
