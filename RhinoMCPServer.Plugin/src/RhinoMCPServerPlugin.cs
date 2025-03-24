5613fe13dfb2dcb2a210ca63412b2056c762b5e9
4kk11 <akiaki118row@yahoo.co.jp>
2025-03-25 01:37:57 +0900

    Add: manual assembly load

--- RhinoMCPServer.Plugin/src/RhinoMCPServerPlugin.cs
+++ RhinoMCPServer.Plugin/src/RhinoMCPServerPlugin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Rhino;
 
 namespace RhinoMCPServer.Plugin
@@ -9,8 +11,41 @@ namespace RhinoMCPServer.Plugin
         {
             Instance = this;
             Console.WriteLine("RhinoMCPServerPlugin");
+            
+            // アセンブリ解決イベントを設定
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
         }
         
         public static RhinoMCPServerPlugin? Instance { get; private set; }
+
+        private Assembly? OnAssemblyResolve(object? sender, ResolveEventArgs args)
+        {
+            try
+            {
+                // 要求されたアセンブリの名前を取得
+                var assemblyName = new AssemblyName(args.Name);
+                
+                // プラグインのインストールディレクトリを取得
+                var pluginPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (pluginPath == null) return null;
+
+                // Toolsディレクトリ内のDLLを探す
+                var toolsDllPath = Path.Combine(pluginPath, "Tools", $"{assemblyName.Name}.dll");
+                
+                // DLLが存在する場合はロード
+                if (File.Exists(toolsDllPath))
+                {
+                    Console.WriteLine($"Loading assembly from Tools directory: {toolsDllPath}");
+                    return Assembly.LoadFrom(toolsDllPath);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in assembly resolution: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
