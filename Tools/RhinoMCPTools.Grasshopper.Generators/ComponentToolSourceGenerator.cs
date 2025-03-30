df230b4058c255b305189b4393826fabe57c216f
4kk11 <akiaki118row@yahoo.co.jp>
2025-03-31 01:16:00 +0900

    Update: separated source text template

--- Tools/RhinoMCPTools.Grasshopper.Generators/ComponentToolSourceGenerator.cs
+++ Tools/RhinoMCPTools.Grasshopper.Generators/ComponentToolSourceGenerator.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Diagnostics;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
-
+using RhinoMCPTools.Grasshopper.Generators.Templates;
 
 namespace RhinoMCPTools.Grasshopper.Generators
 {
@@ -17,7 +15,6 @@ namespace RhinoMCPTools.Grasshopper.Generators
 
         public void Execute(GeneratorExecutionContext context)
         {
-            // Debugger.Launch();
             var analyzer = new ComponentAnalyzer(context);
             var componentTypes = analyzer.FindAllGrasshopperComponents();
         
@@ -28,134 +25,8 @@ namespace RhinoMCPTools.Grasshopper.Generators
                 var className = componentType.Name;
                 var toolClassName = $"{className}Tool";
                 var toolName = $"create_{className.ToLower()}_component";
-        
-                var source = $@"
-        #nullable enable
-        using System;
-        using System.Linq;
-        using System.Threading.Tasks;
-        using System.Text.Json;
-        using Grasshopper;
-        using Grasshopper.Kernel;
-        using ModelContextProtocol.Protocol.Types;
-        using ModelContextProtocol.Server;
-        using RhinoMCPServer.Common;
-        using {namespaceName};
-        using Rhino;
-        
-        namespace GHComponentSourceGenerator
-        {{
-            public class {toolClassName} : IMCPTool
-            {{
-                public string Name => ""{toolName}"";
-        
-                public string Description {{ get; }}
-        
-                public JsonElement InputSchema => JsonSerializer.Deserialize<JsonElement>(@""{{
-                    """"type"""": """"object"""",
-                    """"properties"""": {{
-                        """"x"""": {{
-                            """"type"""": """"number"""",
-                            """"description"""": """"The x-coordinate position on the canvas"""",
-                            """"default"""": 0
-                        }},
-                        """"y"""": {{
-                            """"type"""": """"number"""",
-                            """"description"""": """"The y-coordinate position on the canvas"""",
-                            """"default"""": 0
-                        }}
-                    }}
-                }}"");
-        
-                public {toolClassName}()
-                {{
-                    var component = new {className}();
-                    Description = component.Description;
-                }}
-        
-                public Task<CallToolResponse> ExecuteAsync(CallToolRequestParams request, IMcpServer? server)
-                {{
-                    try
-                    {{
-                        // Get position parameters
-                        var x = request.Arguments?.TryGetValue(""x"", out var xValue) == true ?
-                            Convert.ToDouble(xValue.ToString()) : 0.0;
-                        var y = request.Arguments?.TryGetValue(""y"", out var yValue) == true ?
-                            Convert.ToDouble(yValue.ToString()) : 0.0;
-        
-                        // Get active Grasshopper document
-                        GH_Document? doc = Instances.ActiveDocument;
-                        if (doc == null)
-                        {{
-                            throw new McpServerException(""No active Grasshopper document found"");
-                        }}
-        
-                        // Create and add component
-                        var component = new {className}();
-                        doc.AddObject(component, false);
-                        component.Attributes.Pivot = new System.Drawing.PointF((float)x, (float)y);
-        
-                        // Force complete solution recalculation to update UI
-                        component.ExpireSolution(true);
-                        RhinoApp.InvokeOnUiThread(() =>
-                        {{
-                            Instances.RedrawCanvas();
-                        }});
-        
-                        var response = new
-                        {{
-                            status = ""success"",
-                            component = new
-                            {{
-                                guid = component.InstanceGuid.ToString(),
-                                name = component.Name,
-                                position = new
-                                {{
-                                    x = x,
-                                    y = y
-                                }},
-                                parameters = new
-                                {{
-                                    input = component.Params.Input.Select(p => new
-                                    {{
-                                        name = p.Name,
-                                        nickname = p.NickName,
-                                        description = p.Description,
-                                        type_name = p.TypeName,
-                                        param_id = p.InstanceGuid.ToString()
-                                    }}).ToArray(),
-                                    output = component.Params.Output.Select(p => new
-                                    {{
-                                        name = p.Name,
-                                        nickname = p.NickName,
-                                        description = p.Description,
-                                        type_name = p.TypeName,
-                                        param_id = p.InstanceGuid.ToString()
-                                    }}).ToArray()
-                                }}
-                            }}
-                        }};
-        
-                        return Task.FromResult(new CallToolResponse()
-                        {{
-                            Content = [new Content()
-                            {{
-                                Text = JsonSerializer.Serialize(response, new JsonSerializerOptions
-                                {{
-                                    WriteIndented = true
-                                }}),
-                                Type = ""text""
-                            }}]
-                        }});
-                    }}
-                    catch (Exception ex)
-                    {{
-                        throw new McpServerException($""Error creating {className} component: {{ex.Message}}"", ex);
-                    }}
-                }}
-            }}
-}}";
 
+                var source = ComponentToolTemplate.GetTemplate(namespaceName, className, toolClassName, toolName);
                 context.AddSource($"{toolClassName}.g.cs", SourceText.From(source, Encoding.UTF8));
             }
         }
